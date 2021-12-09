a37de7b251fe31ea1ae83ddf4888aee22c399fd0
he <bgyhenrietta@yahoo.com>
2021-12-09 11:32:50 +0100

    uniquefinder

---
+++ sv2-2021-jvjbf-mintavizsga/src/main/java/uniquefinder/UniqueFinder.java
@@ -0,0 +1,22 @@
+package uniquefinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueFinder {
+
+    public List<Character> getUniqueChars(String words) {
+        try {
+            char[] input = words.toCharArray();
+            List<Character> expected = new ArrayList<>();
+            for (char actual : input) {
+                if ( !expected.contains(actual)) {
+                    expected.add(actual);
+                }
+            }
+            return expected;
+        } catch (NullPointerException npe) {
+            throw new IllegalArgumentException("Text is null.");
+        }
+    }
+}
